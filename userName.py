b8eae3b7ffefa53243f13a3fbc6545c3d20e739b
Rex171 <Rex171@users.noreply.github.com>
2020-02-14 11:54:13 +0500

    Update userName.py

--- userName.py
+++ userName.py
@@ -1,7 +1,13 @@
 import getpass
 import os
 a = getpass.getuser()
-directory = 'C:/Users/'+a
-files = os.listdir(directory) 
+#directory = 'C:/Users/'+a
+#files = os.listdir(directory) 
 print(a)
-print(os.getcwd)
+#получение древа каталогов
+for i in os.listdir('/Users/'+a):
+    print(i)
+#получение директории
+dir = os.path.abspath(os.curdir)
+print('Текущий пользователь: ' + a)
+print('Абсолютный путь к дериктории в которой запущен скрипт: '+dir)
