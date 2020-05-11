3d884d8333a655f8973ec7669f91d16c952d8911
Rex171 <59225620+Rex171@users.noreply.github.com>
2020-05-11 23:16:54 +0500

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-put = input('Привет! Введи путь к файлу который хочешь копировать: ')
+put = input('Введите путь к файлу который хотите скопировать: ')
 err = os.path.exists('{}'.format(put))
 if err == True:
-    put2 = input('Отлично! А теперь введите путь куда будем копировать: ')
+    put2 = input('Введите путь куда копировать: ')
     err2 = os.path.exists('{}'.format(put2))
     if err2 == True:
         shutil.copy('{}'.format(put), '{}'.format(put2))
