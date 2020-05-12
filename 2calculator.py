9396553cda8e9b3384bf5f03bbdc4387028a0e8d
Rex171 <59225620+Rex171@users.noreply.github.com>
2020-05-12 11:05:01 +0500

    Update 2calculator.py

--- 2calculator.py
+++ 2calculator.py
@@ -5,18 +5,18 @@ def addBinary(a,b):
   sumx = int(a, 2) + int(b, 2)
   return bin(sumx)[2:]
 def minusBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа'+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа'+ ch
+  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
+  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
   minx = int(a, 2) - int(b, 2)
   return bin(minx)[2:]
 def multiBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа'+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа'+ ch
+  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
+  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
   mulx = int(a, 2) * int(b, 2)
   return bin(mulx)[2:]
 def delBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа'+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа'+ ch
+  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
+  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
   delx = int(a, 2) - int(b, 2)
   return bin(delx)[2:]
 
