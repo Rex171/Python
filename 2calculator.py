fd583f9916483522cb52d99cab65b8157743d809
Rex171 <59225620+Rex171@users.noreply.github.com>
2020-05-19 13:13:46 +0500

    Update 2calculator.py

--- 2calculator.py
+++ 2calculator.py
@@ -1,30 +1,13 @@
-
-def addBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
-  sumx = int(a, 2) + int(b, 2)
-  return bin(sumx)[2:]
-def minusBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
-  minx = int(a, 2) - int(b, 2)
-  return bin(minx)[2:]
-def multiBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
-  mulx = int(a, 2) * int(b, 2)
-  return bin(mulx)[2:]
-def delBinary(a,b):
-  for ch in a: assert ch in {'0','1'}, 'не верные числа '+ ch
-  for ch in b: assert ch in {'0','1'}, 'не верные числа '+ ch
-  delx = int(a, 2) - int(b, 2)
-  return bin(delx)[2:]
-
-a = input("Введите 1 число: ")
-b = input("Введите 2 число: ")
-
-d = addBinary(a,b)
-d1 = minusBinary(a,b)
-d2 = multiBinary(a,b)
-d3 = delBinary(a,b)
-print(d,d1,d2,d3)
+n1 = input("Введите первое двоичное число: ")
+n2 = input("Введите второе двоичное число: ")
+#  преобразование из десятичной системы в двоичную
+n1 = int(n1,2)
+n2 = int(n2,2)
+ 
+bit_or = n1 | n2
+bit_and = n1 & n2
+bit_xor = n1 ^ n2
+ 
+print("Результат побитового OR: %10s" % bin(bit_or))
+print("Результат побитового AND: %10s" % bin(bit_and))
+print("Результат побитового XOR: %10s" % bin(bit_xor))
